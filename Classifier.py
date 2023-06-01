fd8f87ddc611939536ac860e9f5d00deb560acde
Gabriel Catalin Ionescu <gabescu05@gmail.com>
2023-06-01 22:59:32 +0200

    oops the duplicated files

--- Classifier.py
+++ Classifier.py
@@ -1,51 +1,27 @@
-from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import pickle
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, f1_score, recall_score, classification_report
+import pickle
+import Feature_Extractor as f_extract
+
+def classify(image_path, image_mask_path):
+    width, height, diameter, perimeter_pixel, area, compactness, symmetry_pixel, colors_cancerous = f_extract.get_features(image_path, image_mask_path)
+    
+    features = [width, height, diameter, perimeter_pixel, area, compactness, symmetry_pixel]
+    features.extend(colors_cancerous[0])
+    features.extend(colors_cancerous[1])
+    features.extend(colors_cancerous[2])
+    features = np.reshape(features, (1, -1))
+    
+    classifier = pickle.load(open("Final_Model5", "rb")) 
+    probability = classifier.predict_proba(features)
+    label = classifier.predict(features)
+    
+    return label, probability
+    
 
 def main():
-    data = pd.read_csv('features.csv')
-
-    X = data[data.columns[0:-1]] 
-    y = data[data.columns[-1]]
-    X,y = X.to_numpy(), y.to_numpy()
-    y = np.ravel(y)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size = 0.20)
-    X_train, X_val, y_train, y_val  = train_test_split(X_train, y_train, test_size = 0.25)
-
-    classifier = KNeighborsClassifier(n_neighbors = 9)
-
-    classifier.fit(X_train, y_train)
-    predictions = classifier.predict(X_val)
-    predictions_proba = classifier.predict_proba(y_val)
-
-    accuracy = accuracy_score(y_val, predictions) 
-    recall = recall_score(y_val, predictions) 
-    f1 = f1_score(y_val, predictions) 
-
-    print("\n")
-    print(accuracy)
-    print(recall)
-    print(f1)
-    
-    predicted_label = np.where(predictions_proba[:,0] > 0.5, 0, 1)
-    conf_matrix = confusion_matrix(y_test, predicted_label, labels = classifier.classes_, normalize = "true")
-
-    display_conf = ConfusionMatrixDisplay(confusion_matrix = conf_matrix, display_labels = classifier.classes_)
-    display_conf.plot()
-    plt.show()
-    
-    print(classification_report(y_val, predictions))
-    
-    model2 = pickle.load(open("Final_Model5", "rb"))
-    print(model2.score(X_test, y_test)) 
-    
-
-
+    sample_imagee = "PAT_191_294_629.png"
+    sample_mask = "PAT_191_294_629_Mask.png"
+    print(classify(sample_imagee, sample_mask))
 
 if __name__ == "__main__":
     main()
